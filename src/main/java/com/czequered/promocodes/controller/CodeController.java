3b5a302fea2da57f3bbe3878f5966343a727acb2
sm4 <sm4@kul.is>
2017-02-12 10:49:08 +1100

    Reconfigured CORS

--- src/main/java/com/czequered/promocodes/controller/CodeController.java
+++ src/main/java/com/czequered/promocodes/controller/CodeController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,8 +20,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  * @author Martin Varga
  */
 @RestController
-@RequestMapping("/v1/games/{gameId}/codes")
-@CrossOrigin
+@RequestMapping("/api/v1/games/{gameId}/codes")
 public class CodeController {
 
     private CodeService service;
