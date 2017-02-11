ebaa0332e34b2687cfd4cbae97fa7ed606dc1e66
sm4 <sm4@kul.is>
2017-02-11 22:20:25 +1100

    Allowed CORS

--- src/main/java/com/czequered/promocodes/controller/CodeController.java
+++ src/main/java/com/czequered/promocodes/controller/CodeController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  */
 @RestController
 @RequestMapping("/v1/games/{gameId}/codes")
+@CrossOrigin
 public class CodeController {
 
     private CodeService service;
