022ef2be6fd4436e71dd9d493a1a51cb73297d21
Martin Varga <m.varga@pks.com.au>
2017-02-14 09:05:40 +1100

    Added game test

---
+++ src/test/java/com/czequered/promocodes/model/GameTest.java
@@ -0,0 +1,36 @@
+package com.czequered.promocodes.model;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Martin Varga
+ */
+public class GameTest {
+    @Test
+    public void equalsTest() throws Exception {
+        Game game1 = new Game();
+        game1.setGameId("test");
+        game1.setUserId("Joe");
+        game1.setDetails("{}");
+
+        Game game2 = new Game();
+        game2.setGameId("test");
+        game2.setUserId("Joe");
+        game2.setDetails("Not empty");
+
+        assertThat(game1).isEqualTo(game2);
+    }
+
+    @Test
+    public void hashGameTest() throws Exception {
+        Game game1 = new Game();
+        game1.setGameId("test");
+        game1.setUserId("Joe");
+        game1.setDetails("A");
+
+        GameCompositeId id = new GameCompositeId("Joe", "test");
+        assertThat(game1.hashCode()).isEqualTo(id.hashCode());
+    }
+}
