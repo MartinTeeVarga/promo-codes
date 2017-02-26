d13e89aa5e5e85465e8ce49b1cf583512ca1b776
sm4 <sm4@kul.is>
2017-02-26 20:54:14 +1100

    Tutorial, in progress

--- src/main/java/com/czequered/promocodes/config/SecurityConfig.java
+++ src/main/java/com/czequered/promocodes/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.czequered.promocodes.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author Martin Varga
  */
 @Configuration
-@EnableOAuth2Sso
+@EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
