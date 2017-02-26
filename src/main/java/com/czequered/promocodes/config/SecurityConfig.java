9c2c11a34bfceb9aa1f3cdc21e9533fac201b848
sm4 <sm4@kul.is>
2017-02-26 21:42:11 +1100

    Tutorial, in progress

--- src/main/java/com/czequered/promocodes/config/SecurityConfig.java
+++ src/main/java/com/czequered/promocodes/config/SecurityConfig.java
@@ -1,12 +1,8 @@
 package com.czequered.promocodes.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author Martin Varga
@@ -14,22 +10,74 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @Configuration
 @EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    OAuth2ClientContext oauth2ClientContext;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-            .antMatcher("/**")
-            .authorizeRequests()
-                .antMatchers("/", "/login**", "/webjars/**")
-                .permitAll()
-            .anyRequest()
-                .authenticated()
-            .and().logout().logoutSuccessUrl("/").permitAll()
-            .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-        // @formatter:on
-    }
+    //TODO copy from the real example
+//
+//    @Autowired
+//    OAuth2ClientContext oauth2ClientContext;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // @formatter:off
+//        http
+//            .antMatcher("/**")
+//            .authorizeRequests()
+//                .antMatchers("/", "/login**", "/webjars/**")
+//                .permitAll()
+//            .anyRequest()
+//                .authenticated()
+//            .and().logout().logoutSuccessUrl("/").permitAll()
+//            .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//            .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+//        // @formatter:on
+//    }
+//
+//    private Filter ssoFilter() {
+//
+//        CompositeFilter filter = new CompositeFilter();
+//        List<Filter> filters = new ArrayList<>();
+//
+//        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+//        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+//        facebookFilter.setRestTemplate(facebookTemplate);
+//        UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebook.getUserInfoUri(), facebook().getClientId());
+//        tokenServices.setRestTemplate(oAuth2RestTemplate);
+//        facebookFilter.setTokenServices(tokenServices);
+//        filters.add(facebookFilter);
+//
+//        OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
+//        OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(github(), oauth2ClientContext);
+//        githubFilter.setRestTemplate(githubTemplate);
+//        tokenServices = new UserInfoTokenServices(github().getUserInfoUri(), github().getClientId());
+//        tokenServices.setRestTemplate(oAuth2RestTemplate);
+//        githubFilter.setTokenServices(tokenServices);
+//        filters.add(githubFilter);
+//
+//        filter.setFilters(filters);
+//        return filter;
+//
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties("facebook.client")
+//    public AuthorizationCodeResourceDetails facebook() {
+//        return new AuthorizationCodeResourceDetails();
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties("facebook.resource")
+//    public ResourceServerProperties facebookResource() {
+//        return new ResourceServerProperties();
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties("github.client")
+//    public AuthorizationCodeResourceDetails github() {
+//        return new AuthorizationCodeResourceDetails();
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties("github.resource")
+//    public ResourceServerProperties githubResource() {
+//        return new ResourceServerProperties();
+//    }
 }
