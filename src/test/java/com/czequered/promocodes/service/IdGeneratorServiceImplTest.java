0729bb850a9dc5344b755f0b65e35be98b4ab2d1
sm4 <sm4@kul.is>
2017-03-18 21:24:04 +1100

    ensured that the generated id has size 26 always

--- src/test/java/com/czequered/promocodes/service/IdGeneratorServiceImplTest.java
+++ src/test/java/com/czequered/promocodes/service/IdGeneratorServiceImplTest.java
@@ -12,7 +12,7 @@ public class IdGeneratorServiceImplTest {
     public void generate() throws Exception {
         IdGeneratorService idGeneratorService = new IdGeneratorServiceImpl();
         assertThat(idGeneratorService.generate())
-            .hasSize(26);
+                .hasSize(26);
     }
 
 }
